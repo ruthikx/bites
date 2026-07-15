5fae40f52713ed135aeb09f5df494ca92089d760
ruthikx <rruthik355@gmail.com>
2026-07-15 21:53:16 +0530

    final commit

--- middlewares/validate.ts
+++ middlewares/validate.ts
@@ -1,12 +1,14 @@
-import type { Request,Response,NextFunction } from "express";
-import type { ZodType } from "zod";
+import type { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
 
-export const validate = <T>(schema: ZodType<T>) => (req: Request, res: Response, next: NextFunction) => {
+export const validate =
+  <T>(schema: ZodSchema<T>) =>
+  (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
-    if(!result.success) {
-        return res 
-            .status(400)
-            .json({success:false, errors: result.error.issues})
+    if (!result.success) {
+      return res
+        .status(400)
+        .json({ success: false, errors: result.error.errors });
     }
     next();
-}
+  };
--- utils/responses.ts
+++ utils/responses.ts
@@ -1,13 +1,13 @@
 import type { Response } from "express";
 
 export function successResponse(
-    res: Response, 
-    data: any, 
-    message: string = "Success"
+  res: Response,
+  data: any,
+  message: string = "Success"
 ) {
-    return res.status(200).json({success : true, message, data})
+  return res.status(200).json({ success: true, message, data });
 }
 
-export function errorResponse(res: Response, status: number, error: string){
-    return res.status(status).json({success: false, error})
+export function errorResponse(res: Response, status: number, error: string) {
+  return res.status(status).json({ success: false, error });
 }
